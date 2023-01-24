23030ebf3e98a15a99415d20b373608dd52c1938
Marcelo1080p <juniooor.2015@gmail.com>
2023-01-24 20:30:25 -0300

    adicionando animation background

--- src/Components/ContainerImg.jsx
+++ src/Components/ContainerImg.jsx
@@ -2,23 +2,36 @@ import styled, { keyframes } from "styled-components";
 
 const animeImg = keyframes`
     from {
-        transform: translateX(5px);
+        transform: translate(-5px, 5px);
+        
     }
     to {
-        transform: translateX(-5px);
+        transform: translate(5px, -5px);
+    }
+`
+
+
+const backgroundAnime = keyframes`
+    from {
+        background-position: 0 50%;
+    }
+
+    to {
+        background-position: 100% 0;
     }
 `
 
 export const ContainerImg = styled.div`
     position: relative;
-    border: 1px solid;
     display: flex;
     justify-content: center;
     align-items: center;
+    border-radius: 8px;
     width: 250px;
     height: 250px;
-    background-color: #00000021;
+    box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
     overflow: hidden;
+
     
     
     &::before, &::after {
@@ -26,8 +39,10 @@ export const ContainerImg = styled.div`
         content: "";
         width: 50%;
         height: 80%;
-        background-color: #000;
-        filter: blur(10px);
+        background: linear-gradient(to right, #536976, #292e49);
+        background-size: 300% 300%;
+        animation: ${backgroundAnime} 5s infinite alternate;
+        filter: blur(10px); 
     }
     
     &::before {
@@ -42,6 +57,7 @@ export const ContainerImg = styled.div`
         position: relative;
         width: 90%;
         z-index: 1;
+        border-radius: 8px;
         animation: ${animeImg} 2s infinite alternate ease-in-out;
     }
 
