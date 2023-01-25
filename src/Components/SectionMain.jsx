859ff7c37f0483286ca49e95116fa5b96fdd2a0c
Marcelo1080p <juniooor.2015@gmail.com>
2023-01-24 21:44:18 -0300

    adicionado blur nos pseudos elementos da sectionMain

--- src/Components/SectionMain.jsx
+++ src/Components/SectionMain.jsx
@@ -10,4 +10,24 @@ export const SectionMain = styled.main`
     width: 100%;
     height: 700px;
     border: 1px solid #000;
+    overflow: hidden;
+
+
+    &::before, &::after {
+        position: absolute;
+        content: "";
+        filter: blur(150px);
+        background: linear-gradient(90deg, #2a0845, aqua);
+        border-radius: 50%;
+        height: 360px;
+        width: 360px;
+    }
+    &::before {
+        top: 10%;
+        left: 10%;
+    }
+    &::after {
+        top: 50%;
+        left: 70%;
+    }
 `
